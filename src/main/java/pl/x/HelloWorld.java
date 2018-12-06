5687235cfbba5b21a95cf6096db0cc28a139040c
Maettay <m.wilczynski@op.pl>
2018-12-06 19:56:08 +0100

    ttttt

--- src/main/java/pl/x/HelloWorld.java
+++ src/main/java/pl/x/HelloWorld.java
@@ -3,6 +3,7 @@ package pl.x;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("co innnnnnnnnnnego");
     }
 
 
