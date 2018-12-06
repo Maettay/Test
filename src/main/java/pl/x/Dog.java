4dc05e596c73292847dcda474fa4f218f4dd61b9
Maettay <m.wilczynski@op.pl>
2018-12-06 19:13:29 +0100

    [add] second /new class Dog/

---
+++ src/main/java/pl/x/Dog.java
@@ -0,0 +1,11 @@
+package pl.x;
+
+public class Dog {
+    private String name;
+    private String surname;
+
+    public Dog(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
